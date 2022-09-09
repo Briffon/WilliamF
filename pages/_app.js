7db7fffbead2cea8f21a1f216fe29aa0b11f2438
Britton <ashyoda@gmail.com>
2022-09-09 19:47:26 -0400

    made footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 // import '../styles/globals.css'
 import '../styles/main.scss'
 import Nav from '../components/nav/index'
+import Footer from '../components/footer'
 
 function MyApp({ Component, pageProps }) {
   // return <Component {...pageProps} />
   return <>
     <Nav />
     <Component {...pageProps} />
+    <Footer />
   </>
 }
 
